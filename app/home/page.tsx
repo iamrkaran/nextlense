65d445769851425c0431617eb2f6ee4b5f39d5a6
iamClutchX <iamr.karan@gmail.com>
2023-07-13 13:01:57 +0530

    tried to fix posts and comments

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,77 +1,88 @@
-"use client";
-import { Session } from "next-auth";
-import { useParams, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { getSession, signOut } from "next-auth/react";
-import { User } from "next-auth";
+"use client"
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useSession } from "next-auth/react";
 import Layout from "@/components/Layout";
 import axios from '@/config/axiosInstance';
-import Post from "@/components/Post";
+import PostComponent from "@/components/Post";
 import { HashLoader } from "react-spinners";
-
-interface CustomUser extends User {
-  username: string;
-  accessToken: string;
-}
-
-interface CustomSession extends Session {
-  user: CustomUser;
-  accessToken: string;
-}
+import { revalidateTag } from "next/cache";
+import next from "next";
 
 type Post = {
   _id: string;
   user: string;
-  caption: string;
+  caption: string; 
   image: string;
   createdDate: Date;
   likes: string[];
   comments: { user: string, text: string, createdDate: Date }[];
 };
 
-const HomePage = () => {
+type Session = ReturnType<typeof useSession>['data'];
+
+const HomePage = ({ initialPosts }: { initialPosts: Post[] }) => {
   const router = useRouter();
-  const { username } = useParams();
+  const { data: session, status: sessionStatus } = useSession();
+  const [posts, setPosts] = React.useState<Post[]>(initialPosts);
+  const [postLoading, setPostLoading] = React.useState<boolean>(false);
 
-  const [session, setSession] = useState<CustomSession | null>(null);
-  const [posts, setPosts] = useState<Post[]>([]); // Create a state for the posts
-  const [isLoading, setIsLoading] = useState(true); // Create a state for loading
+  const fetchPosts = () => {
+    if (!session) return;
 
-  useEffect(() => {
-    getSession().then((session: any) => {
-      setSession(session);
-    });
-
+    setPostLoading(true);
     axios
       .get('/posts')
-      .then(response => {
-        setPosts(response.data);
-        setIsLoading(false); // Set loading to false when data is fetched
+      .then((res) => {
+        setPosts(res.data);
+        setPostLoading(false);
+        revalidateTag('posts');
       })
-      .catch(error => {
-        console.error(error);
-        setIsLoading(false); // Set loading to false in case of an error
+      .catch((err) => {
+        setPostLoading(false);
       });
+  };
 
-  }, []);
+  // Fetch posts initially when the session changes
+  React.useEffect(() => {
+    fetchPosts();
+  }, [session]);
+
+  if (sessionStatus === 'loading' || postLoading) {
+    return (
+      <div className="flex justify-center items-center h-64">
+        <HashLoader color="#36d7b7" />
+      </div>
+    );
+  }
 
   return (
     <Layout>
-      {isLoading ? (
-        <div className="flex justify-center items-center h-64">
-          <HashLoader color="#36d7b7" />
-        </div>
-      ) : (
-        <div className="text-center bg-gray-900 min-h-screen px-4 sm:px-6 lg:px-8">
-          <div className="py-12 mb-12">
-            {posts.map((post) => (
-              <Post key={post._id} post={post} session={session} />
-            ))}
-          </div>
-        </div>
-      )}
-    </Layout>
+    <div className="text-center bg-gray-900 min-h-screen px-4 sm:px-6 lg:px-8">
+      <div className="py-12 mb-12">
+        {posts && posts.map((post: Post) => (
+          <PostComponent key={post._id} post={post} session={session as Session} />
+        ))}
+      </div>
+    </div>
+  </Layout>
   );
 };
+
+export async function getServerSideProps(_ : any) {
+  let initialPosts = [];
+  try {
+    const res = await axios.get('/posts');
+    initialPosts = res.data;
+  } catch (err) {
+    console.error(err);
+  }
+
+  return {
+    props: {
+      initialPosts,
+    },
+  };
+}
 
 export default HomePage;
--- components/AddComment.tsx
+++ components/AddComment.tsx
@@ -1,54 +1,78 @@
-
 import React, { useState, FormEvent } from "react";
 import axios from "@/config/axiosInstance";
-import InputEmoji from "react-input-emoji"; 
+import InputEmoji from "react-input-emoji";
+import { revalidateTag } from "next/cache";
 
 interface AddCommentProps {
-    postId: string;
-    session: any;
+  postId: string;
+  session: any;
 }
 
 const AddComment: React.FC<AddCommentProps> = ({ postId, session }) => {
-    const [comment, setComment] = useState<string>('');
+  const [comment, setComment] = useState<string>('');
 
-    const handleCommentChange = (emojiText: string) => {
-        setComment(emojiText);
-    };
+  const handleCommentChange = (emojiText: string) => {
+    setComment(emojiText);
+  };
 
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        try {
-            await axios.post('/posts/comment', {
-                postId,
-                userId: session?.user?.id,
-                comment
-            });
-            setComment('');
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      await axios.post('/posts/comment', {
+        postId,
+        userId: session?.user?.id,
+        comment
+      });
+      setComment('');
+      revalidateTag('posts');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="flex items-center w-full border-b-0 focus-within:border-b focus-within:border-blue-500">
-            <InputEmoji
-                value={comment}
-                onChange={handleCommentChange}
-                cleanOnEnter
-                theme="dark"
-                borderColor="transparent"
-                borderRadius={0}
-                placeholder="Add a comment..."
-                className=" h-10 outline-none border-none border-b border-gray-300 focus:border-blue-500 resize-none overflow-hidden"
-            />
+  return (
+    <form onSubmit={handleSubmit} className="flex items-center w-full border-b-0 focus-within:border-b focus-within:border-blue-500">
+      <InputEmoji
+        value={comment}
+        onChange={handleCommentChange}
+        cleanOnEnter
+        theme="dark"
+        borderColor="transparent"
+        borderRadius={0}
+        placeholder="Add a comment..."
+        className="h-10 outline-none border-none border-b border-gray-300 focus:border-blue-500 resize-none overflow-hidden"
+      />
 
-            {comment ? (
-                <button type="submit" className="px-2 text-blue-500">Post</button>
-            ) : (
-                <span>&nbsp; &nbsp;</span>
-            )}
-        </form>
-    );
+      {comment ? (
+        <button type="submit" className="px-2 text-blue-500">Post</button>
+      ) : (
+        <span>&nbsp; &nbsp;</span>
+      )}
+    </form>
+  );
 };
+
+export async function fetchInitialComments(postId: string) {
+    try {
+      const response = await axios.get(`/posts/${postId}/comments`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+export async function getServerSideProps(_: any, context: any) {
+   
+    const postId = context.params?.postId as string;
+    const comments = await fetchInitialComments(postId);
+  
+    return {
+      props: {
+        postId,
+        comments,
+      },
+    };
+  }
 
 export default AddComment;
